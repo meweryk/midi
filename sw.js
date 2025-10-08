f30f047b8a375b7b6e3bd28b10117ed52baa30b7
meweryk <39477539+meweryk@users.noreply.github.com>
2025-10-08 14:19:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'midi-bells-v9.6';
-const DYNAMIC_CACHE = 'dynamic-cache-v9.6';
+const CACHE_NAME = 'midi-bells-v9.61';
+const DYNAMIC_CACHE = 'dynamic-cache-v9.61';
 const FALLBACK_HTML = '/midi/index.html';
 const FALLBACK_IMAGE = '/midi/icon-192.png';
 
