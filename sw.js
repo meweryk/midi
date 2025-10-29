5005e7a4ee4cce6d37aa4ed3fa1db4d2c783eea6
meweryk <39477539+meweryk@users.noreply.github.com>
2025-10-30 01:35:48 +0200

    Update cache version to v9.67

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'midi-bells-v9.66';
-const DYNAMIC_CACHE = 'dynamic-cache-v9.66';
+const CACHE_NAME = 'midi-bells-v9.67';
+const DYNAMIC_CACHE = 'dynamic-cache-v9.67';
 const FALLBACK_HTML = '/midi/index.html';
 const FALLBACK_IMAGE = '/midi/icon-192.png';
 
