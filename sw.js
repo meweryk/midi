6010085186d80ca4e361fb92e84b2458fe5c4c7c
meweryk <iliya.pavlov1109@gmail.com>
2026-02-13 15:37:25 +0200

    update sw.js

--- sw.js
+++ sw.js
@@ -146,3 +146,9 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
