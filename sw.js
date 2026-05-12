1fd3501e7af5480b511f2c529a6cac6e488f89d3
meweryk <iliya.pavlov1109@gmail.com>
2026-05-12 22:05:13 +0300

    update range

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'midi-bells-v13.5';
-const DYNAMIC_CACHE = 'dynamic-cache-v13.5';
+const CACHE_NAME = 'midi-bells-v13.6';
+const DYNAMIC_CACHE = 'dynamic-cache-v13.6';
 const FALLBACK_HTML = '/midi/index.html';
 const FALLBACK_IMAGE = '/midi/icon-192.png';
 
